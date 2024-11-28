557b275d10d7dbe5807555ab8859e7c17f59b1be
Mohammed Ansar Usman <mohammedansarusman@gmail.com>
2024-11-28 23:47:13 +0400

    initil

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-3xl text-red-600'>Welcome to react js</h1>
+    </div>
+  );
+}
+
+export default App;
